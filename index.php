9ac820fe2b9fe0843eb1b9cc596c0ddece83750e
Taratarini Patnaik <Taratarini.Patnaik@visbbsr.com>
2021-11-20 16:07:14 +0530

    v1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- end php tag -->
+<?php
+    // Single Line comment
+    #  Single Line comment
+
+    $name =  'Taratarini patnaik';
+    $address = 'bbsr';
+
+    echo $name;
+    echo '<br>'; // '' =>   string
+    echo "<br>"; // "" =>   string
+    echo $address;
+    /*
+        i can write anything inside multiline
+        comment
+        tag
+        you
+        must follwo
+
+        this
+        rule
+    */
+?>
+<?php
+
+
+
+?>
+<?php
+
+
+?>
+<!-- end php tag -->
